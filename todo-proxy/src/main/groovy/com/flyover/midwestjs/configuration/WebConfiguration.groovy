978224dd14a58f520b6c17f942cd9cc6e48ad1b8
Maria.Stommes <maria.stommes@target.com>
2018-08-08 10:18:10 -0500

    initial commit

---
+++ todo-proxy/src/main/groovy/com/flyover/midwestjs/configuration/WebConfiguration.groovy
@@ -0,0 +1,27 @@
+package com.flyover.midwestjs.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration {
+     static final String INDEX_HTML = 'index.html'
+
+    @Bean
+    WebMvcConfigurer forwardToIndex() {
+        new WebMvcConfigurer() {
+            @Override
+            void addViewControllers(ViewControllerRegistry registry) {
+                // Resolve requests to / and /app to index.html so that HTML5 routing works
+                registry
+                        .addRedirectViewController("/", "/app")
+
+                registry
+                        .addViewController("/app/**")
+                        .setViewName(INDEX_HTML)
+            }
+        }
+    }
+}
